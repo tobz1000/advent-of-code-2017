7e7490833ac158459c2c9190669553cf89329548
Toby Dimmick <tobydimmick@protonmail.ch>
2017-12-02 09:52:47 +0000

    Changed file structure

---
+++ 1-2.rs
@@ -0,0 +1,29 @@
+// You notice a progress bar that jumps to 50% completion. Apparently, the door isn't yet satisfied, but it did emit a star as encouragement. The instructions change:
+
+// Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list. That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
+
+// For example:
+
+//     1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+//     1221 produces 0, because every comparison is between a 1 and a 2.
+//     123425 produces 4, because both 2s match each other, but no other digit has a match.
+//     123123 produces 12.
+//     12131415 produces 4.
+
+// What is the solution to your new captcha?
+
+use std::env;
+
+fn main() {
+	let sequence = env::args().nth(1).unwrap();
+	let vals: Vec<u32> = sequence.chars()
+		.map(|c| c.to_digit(10).unwrap())
+		.collect();
+	let offset = vals.len() / 2;
+
+	let ans = vals[..offset].iter().zip(vals[offset..].iter())
+		.filter(|&(a, b)| a == b)
+		.fold(0, |acc, (a, b)| acc + a + b);
+
+	println!("{}", ans);
+}
